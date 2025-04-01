850605ebc26ec6f2802e01e5bdbaa3f524c3ec63
pjh2196 <97954981+pjh2196@users.noreply.github.com>
2025-04-01 14:38:51 +0900

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,14 @@
+# 변수 선언
+name = "Hyemin"
+age = 23
+height = 158.5
+is_student = True
+
+# 출력
+print("Name:", name)
+print("Age:", age)
+print("Height:", height, "cm")
+print("Student:", is_student)
+
+# 간단한 연산
+print("Next year, I will be", age + 1)
